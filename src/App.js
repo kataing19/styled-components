34ae6c722844ab03c6116a4f47e1cc43617e6bce
Kristina Taing <kristina.taing@kiewit.com>
2021-04-19 23:40:19 -0700

    added almost every possible button to the UI

--- src/App.js
+++ src/App.js
@@ -1,31 +1,128 @@
-import "./App.css";
-import styled, { ThemeProvider } from "styled-components";
-import { Button } from './elements/index.js';
-
-const theme = {
-  primary: "teal",
-  secondary: "green",
-  alert: "yellow",
-  font: "sans-serif",
-};
-
-const H1 = styled.h1`
-  font-family: ${(props) => props.theme.font};
-  font-size: 3em;
-`;
+import './App.css';
+import { StyledButton } from './elements/index.js';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <H1>Styled Components</H1>
-        <form action="">
-          <input type="text" />
-          <button>Create</button>
-          <Button color="alert">Create</Button>
-        </form>
+    <div className="App">
+      <div>
+        <h1>Theme</h1>
       </div>
-    </ThemeProvider>
+      <div className="row">
+        <div>
+          <h2>Contained Buttons</h2>
+          <StyledButton variant="contained" color="primary" submit>
+            Contained Primary
+          </StyledButton>
+          <StyledButton variant="contained" color="neutral">
+            Contained Neutral
+          </StyledButton>
+          <StyledButton variant="contained" color="red">
+            Contained Red
+          </StyledButton>
+          <StyledButton variant="contained" color="yellow">
+            Contained Yellow
+          </StyledButton>
+          <StyledButton variant="contained" color="green">
+            Contained Green
+          </StyledButton>
+          <StyledButton variant="contained" color="blue">
+            Contained Blue
+          </StyledButton>
+          <StyledButton variant="contained" color="orange">
+            Contained Orange
+          </StyledButton>
+        </div>
+        <div>
+          <h2>Outlined Buttons</h2>
+          <StyledButton variant="outlined" color="primary">
+            Outlined Primary
+          </StyledButton>
+          <StyledButton variant="outlined" color="neutral">
+            Outlined Neutral
+          </StyledButton>
+          <StyledButton variant="outlined" color="red">
+            Outlined Red
+          </StyledButton>
+          <StyledButton variant="outlined" color="yellow">
+            Outlined Yellow
+          </StyledButton>
+          <StyledButton variant="outlined" color="green">
+            Outlined Green
+          </StyledButton>
+          <StyledButton variant="outlined" color="blue">
+            Outlined Blue
+          </StyledButton>
+          <StyledButton variant="outlined" color="orange">
+            Outlined Orange
+          </StyledButton>
+        </div>
+        <div>
+          <h2>Text Buttons</h2>
+          <StyledButton variant="text" color="primary">
+            Text Primary
+          </StyledButton>
+          <StyledButton variant="text" color="neutral">
+            Text Neutral
+          </StyledButton>
+          <StyledButton variant="text" color="red">
+            Text Red
+          </StyledButton>
+          <StyledButton variant="text" color="yellow">
+            Text Yellow
+          </StyledButton>
+          <StyledButton variant="text" color="green">
+            Text Green
+          </StyledButton>
+          <StyledButton variant="text" color="blue">
+            Text Blue
+          </StyledButton>
+          <StyledButton variant="text" color="orange">
+            Text Orange
+          </StyledButton>
+        </div>
+      </div>
+      <div>
+        <h1>Size</h1>
+      </div>
+      <div className="row">
+        <div>
+          <h2>Contained Buttons</h2>
+          <StyledButton variant="contained" color="primary" size="small">
+            Small Text
+          </StyledButton>
+          <StyledButton variant="contained" color="neutral" size="medium">
+            Medium Text
+          </StyledButton>
+          <StyledButton variant="contained" color="red" size="large">
+            Large Text
+          </StyledButton>
+        </div>
+        <div>
+          <h2>Outlined Buttons</h2>
+          <StyledButton variant="outlined" color="primary" size="small">
+            Small Text
+          </StyledButton>
+          <StyledButton variant="outlined" color="neutral" size="medium">
+            Medium Text
+          </StyledButton>
+          <StyledButton variant="outlined" color="red" size="large">
+            Large Text
+          </StyledButton>
+        </div>
+        <div>
+          <h2>Text Buttons</h2>
+          <StyledButton variant="text" color="primary" size="small">
+            Small Text
+          </StyledButton>
+          <StyledButton variant="text" color="neutral" size="medium">
+            Medium Text
+          </StyledButton>
+          <StyledButton variant="text" color="red" size="large">
+            Large Text
+          </StyledButton>
+        </div>
+      </div>
+    </div>
   );
 }
 
