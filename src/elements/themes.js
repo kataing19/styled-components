04ecb932939972b039546663b3d13ff73183ed1b
Kristina Taing <kristina.taing@kiewit.com>
2021-04-19 21:07:02 -0700

    added a themes.js file

---
+++ src/elements/themes.js
@@ -0,0 +1,93 @@
+// primary color
+export const primary = [
+  'filler',
+  '#d5edff',
+  '#a0cbeb',
+  '#6ba9d7',
+  '#3587c2',
+  '#0065ae',
+  '#00518c',
+  '#003d6a',
+  '#002947',
+  '#001525',
+];
+
+// neutrals
+export const neutral = [
+  'filler',
+  '#FEFEFE',
+  '#F9FAFA',
+  '#F5F5F6',
+  '#F0F1F2',
+  '#E2E4E6',
+  '#D4D7DA',
+  '#C5CACE',
+  '#B7BDC2',
+  '#A9B0B6',
+  '#8E9499',
+  '#73787C',
+  '#595D60',
+  '#3E4143',
+  '#232526',
+  '#080909',
+];
+
+// error
+export const red = [
+  'filler',
+  '#FFECEB',
+  '#FFA09D',
+  '#FF544E',
+  '#FF0800',
+  '#BE0600',
+  '#7C0400',
+  '#3B0200',
+];
+
+// warning
+export const yellow = [
+  'filler',
+  '#FFFCEB',
+  '#FFEE9D',
+  '#FFE14E',
+  '#FFD300',
+  '#CBA800',
+  '#967C00',
+  '#625100',
+];
+
+// success
+export const green = [
+  'filler',
+  '#ECFAEC',
+  '#A9D5A9',
+  '#65B065',
+  '#228B22',
+  '#1C711C',
+  '#155615',
+  '#0F3C0F',
+];
+
+// information
+export const blue = [
+  'filler',
+  '#E6F0FE',
+  '#9BC1F4',
+  '#5092EA',
+  '#0563E0',
+  '#044CAD',
+  '#03367A',
+  '#021F47',
+];
+
+// in progress
+export const orange = [
+  'filler',
+  '#FFEADB',
+  '#FFC39A',
+  '#FF9C58',
+  '#FF7417',
+  '#BF560F',
+  '#7E3808',
+  '#3E1900',
+];
