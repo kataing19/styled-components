4cd08ac3269a06bec18800b106d560e73e3ea7e1
Kristina Taing <kristina.taing@kiewit.com>
2021-04-20 14:46:35 -0700

    renamed to themes to colors and added purple to the palette

---
+++ src/elements/colors.js
@@ -0,0 +1,104 @@
+/*
+  Color Palette
+  - All colors in this file directly correspond with the colors of the Adapt Design System
+  - To view the documentation for all of these colors, visit: 
+      https://portal.kiewit.com/:w:/r/sites/ADAPT/_layouts/15/Doc.aspx?sourcedoc=%7BE3358A2F-4B6C-4D5C-BE14-06AF1F4E4FAB%7D&file=Color.docx&action=default&mobileredirect=true
+*/
+
+// neutrals - used for default button
+const neutral = [
+  'filler',
+  '#FEFEFE',
+  '#F9FAFA',
+  '#F5F5F6',
+  '#F0F1F2',
+  '#E2E4E6',
+  '#D4D7DA',
+  '#C5CACE',
+  '#B7BDC2',
+  '#A9B0B6',
+  '#8E9499',
+  '#73787C',
+  '#595D60',
+  '#3E4143',
+  '#232526',
+  '#080909',
+];
+
+// error
+const red = [
+  'filler',
+  '#FFECEB',
+  '#FFA09D',
+  '#FF544E',
+  '#FF0800',
+  '#BE0600',
+  '#7C0400',
+  '#3B0200',
+];
+
+// in progress
+const orange = [
+  'filler',
+  '#FFEADB',
+  '#FFC39A',
+  '#FF9C58',
+  '#FF7417',
+  '#BF560F',
+  '#7E3808',
+  '#3E1900',
+];
+
+// warning
+const yellow = [
+  'filler',
+  '#FFFCEB',
+  '#FFEE9D',
+  '#FFE14E',
+  '#FFD300',
+  '#CBA800',
+  '#967C00',
+  '#625100',
+];
+
+// success
+const green = [
+  'filler',
+  '#ECFAEC',
+  '#A9D5A9',
+  '#65B065',
+  '#228B22',
+  '#1C711C',
+  '#155615',
+  '#0F3C0F',
+];
+
+// primary & information
+const blue = [
+  'filler',
+  '#d5edff',
+  '#a0cbeb',
+  '#6ba9d7',
+  '#3587c2',
+  '#0065ae',
+  '#00518c',
+  '#003d6a',
+  '#002947',
+  '#001525',
+];
+
+// // old information
+// const blue = [
+//   'filler',
+//   '#E6F0FE',
+//   '#9BC1F4',
+//   '#5092EA',
+//   '#0563E0',
+//   '#044CAD',
+//   '#03367A',
+//   '#021F47',
+// ];
+
+const purple = [];
+
+export default { neutral, red, orange, yellow, green, blue, purple };
